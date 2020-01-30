cea72f0daeec89bc6a2b776729fd96fde688a213
mbronek7 <mbronek7@gmail.com>
2020-01-30 03:01:12 +0100

    Add url param

---
+++ app-monitor.js
@@ -0,0 +1,10 @@
+function send_to_app_monitor(data, url){
+  var request = new XMLHttpRequest();
+  var method = "POST";
+  var shouldBeAsync = true;
+  var shouldBeAsync = true;
+  request.open(method, url, shouldBeAsync);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.send(data);
+}
+
